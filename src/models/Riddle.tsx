be592f2fa8ccf1039be7cd1ee45cb671e3238843
RemiFeydit <remi.feydit@ynov.com>
2020-12-06 21:58:00 +0100

    remaining pop

--- src/models/Riddle.tsx
+++ src/models/Riddle.tsx
@@ -13,6 +13,7 @@ export interface Riddle {
 }
 
 export const defaultRiddles: Riddle[] = [
+<<<<<<< Updated upstream
   {
     id: '1',
     image: 'https://cdn.pixabay.com/photo/2014/04/02/16/16/glue-306757_960_720.png',
@@ -52,3 +53,42 @@ export const defaultRiddles: Riddle[] = [
     component: TreasureHunt,
   }
 ];
+=======
+    {
+        id: '1',
+        image: "https://jojo-app.fr/wp-content/uploads/2018/09/espace-optimise-appartement-meuble-paris.jpg",
+        name: "Riddle 1",
+        difficulty: 3,
+        timeSec: 0,
+        isSuccess: true,
+        path: ""
+    },
+    {
+        id: '2',
+        image: "http://goraguer.fr/wp/wp-content/uploads/2012/06/Appartement-marais-5.jpg",
+        name: "Riddle 2",
+        difficulty: 1,
+        timeSec: 0,
+        isSuccess: false,
+        path: ""
+    },
+    {
+        id: '3',
+        image: "https://blog.malwarebytes.com/wp-content/uploads/2013/11/photodune-5172276-locked-smartphone-m.jpg",
+        name: "Code Lock",
+        difficulty: 1,
+        timeSec: 1,
+        isSuccess: false,
+        path: "/codeLock"
+    },
+    {
+        id:'4',
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Question_mark_%28black%29.svg/600px-Question_mark_%28black%29.svg.png",
+        name: "Another Code ?",
+        difficulty: 1,
+        timeSec: 0,
+        isSuccess: false,
+        path: "/clipboard"
+    }
+]
+>>>>>>> Stashed changes
